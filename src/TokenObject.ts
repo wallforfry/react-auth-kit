37e6ae45a4f919fdbcc12f1ab7ed01a9beed7134
Wallerand Delevacq <wallforfry@gmail.com>
2021-02-11 20:41:00 +0100

    Fallback to expiresAt if refreshTokenExpiresAt is null

--- src/TokenObject.ts
+++ src/TokenObject.ts
@@ -325,7 +325,7 @@ class TokenObject {
     if (this.isUsingRefreshToken && !!this.refreshTokenName &&
       !!refreshToken) {
       Cookies.set(this.refreshTokenName, refreshToken, {
-        expires: refreshTokenExpiresAt,
+        expires: refreshTokenExpiresAt ?? expiresAt,
         domain: this.cookieDomain,
         secure: this.cookieSecure,
       });
@@ -335,7 +335,7 @@ class TokenObject {
       !!refreshTokenExpiresAt) {
       Cookies.set(this.refreshTokenTimeName,
           refreshTokenExpiresAt.toISOString(), {
-            expires: refreshTokenExpiresAt,
+            expires: refreshTokenExpiresAt ?? expiresAt,
             domain: this.cookieDomain,
             secure: this.cookieSecure,
           });
